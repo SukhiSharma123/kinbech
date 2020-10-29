e28886452377ae179d9957ed7fb77d27c901d404
unknown <sharmasukhi123@gmail.com>
2020-10-29 17:18:59 +0545

    logo

--- sellapp/admin.py
+++ sellapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
 from .models import *
 
 
 admin.site.register(
     [Customer, Post, PostImage, Comment])
+
